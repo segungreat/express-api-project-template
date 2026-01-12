31013d2c00d9100aa36527613e4b141aefd7b35d
Oluwasegun Olateju <80651564+segungreat@users.noreply.github.com>
2026-01-12 09:44:53 +0100

    add tsx for development server

--- src/controllers/welcome.controllers.ts
+++ src/controllers/welcome.controllers.ts
@@ -5,3 +5,8 @@ export const welcome = async function (req: Req, res: Res) {
   return CustomSuccessRes.Success(res, 'Welcome to express project template', {});
   // return res.json('Welcome to express project template');
 }
+
+export const login = async function (req: Req, res: Res) {
+  return CustomSuccessRes.Success(res, 'This should be testinge', {});
+  // return res.json('Welcome to express project template');
+}
